9450e7676eef288c15326018a383a8b20d846e95
chouenji <kevin.docanto11229@gmail.com>
2024-05-19 18:48:11 -0300

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>1", ":SearchBoxIncSearch<CR>")
-vim.keymap.set("n", "<leader>2", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "3", "<CMD>NoiceDismiss<CR>")
